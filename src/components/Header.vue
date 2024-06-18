707d3c56110bca9ad88595640e06b6aa7c7e3d5e
eric406-yeboah <ericusyeboah406@gmail.com>
2024-06-18 15:17:46 +0000

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+    <header class="header_section long_section px-0">
+      <nav class="navbar navbar-expand-lg custom_nav-container">
+        <router-link class="navbar-brand" to="/">
+          <span>
+            Edgecut
+          </span>
+        </router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class=""> </span>
+        </button>
+  
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div class="d-flex mx-auto flex-column flex-lg-row align-items-center">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/About">About</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/furniture">Furnitures</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/blog">Blog</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/contact">Contact Us</router-link>
+              </li>
+            </ul>
+          </div>
+          <div class="quote_btn-container">
+            <router-link to="/login">
+              <span>
+                Login
+              </span>
+              <i class="fa fa-user" aria-hidden="true"></i>
+            </router-link>
+            <form class="form-inline">
+              <button class="btn my-2 my-sm-0 nav_search-btn" type="submit">
+                <i class="fa fa-search" aria-hidden="true"></i>
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Header'
+  }
+  </script>
+  
+  <style scoped>
+  /* Add your custom styles here */
+  </style>
+  
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// import Index from '../components/Index.vue'
+import About from '../components/About.vue'
+
+
+const routes = [
+  // { path: '/', component: Index },
+  { path: '/About', component: About },
+ 
+]
+
+const router = createRouter({
+  history: createWebHistory('/'),
+  routes
+})
+
+export default router
